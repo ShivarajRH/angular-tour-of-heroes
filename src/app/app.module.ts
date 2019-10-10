fbcea795042ca1413ccaa63771da8f99edab4cbe
Shivaraja <mrshivaraj123@gmail.com>
2019-10-10 11:28:06 +0530

    20191010-angular-demo-app2- changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRootingModule } from './app-rooting.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRootingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: "Basava"},
+    {id: 12, name: "Kuvempu"},
+    {id: 13, name: "Bahubali"},
+    {id: 14, name: "Shivaji"},
+    {id: 15, name: "Vallabhai"},
+    {id: 16, name: "Rayanna"},
+    {id: 17, name: "Krishnadeva"},
+    {id: 18, name: "Veerabadra"},
+    {id: 19, name: "Allama"},
+    {id: 20, name: "Channabasava"}
+];
